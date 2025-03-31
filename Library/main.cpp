d8c8ae22f954ed4f9dc2f8b62ef4a16f33181109
szynszyl320 <filipdropsik11@o2.pl>
2025-03-31 15:23:53 +0200

    Current state of affairs

---
+++ Library/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+    cout << "Works!" << endl;
+
+    DynamicArray<int> numbers(4);
+
+
+
+    return 0;
+}
---
+++ Library/subfolders/user.class.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+class User
+{
+private:
+    string name;
+    string password;
+    //Book lentBooks;
+    //Book readBooks;
+
+};
